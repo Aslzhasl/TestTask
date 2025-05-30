f7206a78ea097eb7e05f102f37f6b5329fea242a
LAB1 ADS <asek05052005@gmail.com>
2025-05-30 23:17:31 +0500

    comments

--- scripts/3.sql
+++ scripts/3.sql
@@ -2,7 +2,12 @@ create index idx_users_points on users(points desc);
 create index idx_users_city_points on users(city, points desc);
 create index idx_friends_user_id on friends(user_id);
 create index idx_friends_friend_id on friends(friend_id);
-//3.1
+--3.1
+--Оптимизация здесь — это индексы. Индекс по points ускоряет сортировку и поиск топов.
+--Индекс по (city, points) нужен, чтобы топы по городам не тормозили.
+--В friends ставим индексы по user_id и friend_id, чтобы быстро искать друзей и делать join'ы
+
+
 create materialized view top100_users as
 select id, username, city, points
 from users
--- scripts/4.sql
+++ scripts/4.sql
@@ -2,13 +2,16 @@ select id, username, points
 from users
 order by points desc
     limit 100;
-//4.1
+--4.1
+--Тут всё просто: чтобы получить топ пользователей или топ по городу — сортируем по points,
+--лимитируем количество, и всё. Для топа среди друзей просто соединяем таблицы и сортируем.
+--С хорошими индексами всё летает даже на миллионах строк
 select id, username, points
 from users
 where city = 'Дубай'
 order by points desc
     limit 100;
-//4.2
+--4.2
 select u.id, u.username, u.points
 from friends f
          join users u on u.id = f.friend_id
